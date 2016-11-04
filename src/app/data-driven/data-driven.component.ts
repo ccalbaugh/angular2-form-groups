8533b12d2b81d20a934ed81b69f28fdd376a3901
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-04 15:12:48 -0500

    Created new form group in data-driven.component constructor.

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'data-driven',
     templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
+    myForm: FormGroup;
     
+    constructor() {
+        this.myForm = new FormGroup({
+            'username': new FormControl(), // FormControl is what was created automatically in the template approach when attaching ngModel to an input.
+            'email': new FormControl(),
+            'password': new FormCofntrol(),
+        });
+    }
 }
