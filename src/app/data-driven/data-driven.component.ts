634c15f299279fc4a41ef37336ef6c480c625329
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-07 07:57:46 -0600

    Create gender property and radio buttons for gender selection.

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -7,6 +7,11 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataDrivenComponent {
     myForm: FormGroup;
+
+    genders = [
+        'male',
+        'female'
+    ];
     
     constructor() {
         this.myForm = new FormGroup({
@@ -16,7 +21,8 @@ export class DataDrivenComponent {
                     Validators.required, 
                     Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
             }),
-            'password': new FormCofntrol('', Validators.required)
+            'password': new FormControl('', Validators.required),
+            'gender': new FormControl('male')
         });
     }
 
